400048d35b2e4f57454b50941aa3c5a0203d2c3f
Adam Kruchkow <akruchkow@gmail.com>
2016-03-23 03:02:35 -0400

    Cleaned up code

--- testing/resources/transitions.js
+++ testing/resources/transitions.js
@@ -1,32 +1,16 @@
 var page = 0;
+var imageRatio = 0.621;
 
-// Function to change device pages
-function goToPage(num) {
-
-  // Change to a given page
-  $(".page").each(function(){
-    if ($(this).val() == num) {
-      $(this).show();
-    }
-    else {
-      $(this).fadeOut(500);
-    }
-  });
-  
-  // Reduce / Enlarge header
-  var scroll = $("#header").height() * -0.621;
-  $("#header").animate({top: scroll}, 1000);
-  $(".page").animate({top: scroll}, 1000);
-}
-
+// Document functions --- o
 $(document).ready(function() {
 
   // Resize the window
   $(window).resize(function() {
-    if (page != 0) {
-      var scroll = $("#header").height() * -0.621;
-      $("#header").animate({top: scroll}, 0);
-      $(".page").animate({top: scroll}, 0);
+    if (page > 0) {
+      scroll("up",0);
+    }
+    else {
+      scroll("down",0);
     }
   });
 
@@ -36,3 +20,37 @@ $(document).ready(function() {
   });
   
 });
+
+// Helper functions --- o
+function goToPage(num) {
+
+  $(".page").each(function(){
+    if ($(this).val() == num) {
+      $(this).show();
+    }
+    else {
+      $(this).fadeOut(500);
+    }
+  });
+  
+  page = num;
+  
+  if (page > 0) {
+    scroll("up",1000);
+  }
+  else {
+    scroll("down",1000);
+  }
+}
+
+function scroll(direction, duration) {
+  if (direction == "up") {
+      var scroll = $("#header").height() * -imageRatio;
+      $("#header").animate({top: scroll}, duration);
+      $(".page").animate({top: scroll}, duration);
+  }
+  else {
+      $("#header").animate({top: 0}, duration);
+      $(".page").animate({top: 0}, duration);
+  }
+}
