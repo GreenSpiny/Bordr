f6213ace5f0293e685312ad23f764ba6a801b638
Adam Kruchkow <akruchkow@gmail.com>
2016-03-24 15:10:52 -0400

    Small stuff

--- testing/resources/datastructures.js
+++ testing/resources/datastructures.js
@@ -25,16 +25,6 @@ function event() {
   this.participants = "";
 }
 
-// Attempt to sign in a uers.  Returns false if the username and password do not match.
-function loginUser(username, password) {
-
-}
-
-// Attempt to sign up a new user.  Returns false if the username already exists.
-function createUser(username, password) {
-
-}
-
 // Request a user from the database, and deposit the person object into an array
 function getUser(array, username) {
 
